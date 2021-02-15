500a12e5f3802849bf3d9f962be533cefffb27ad
biben-hub <bilombob@gmail.com>
2021-02-15 16:50:55 +0100

    creating the CRUD OP

---
+++ script.js
@@ -0,0 +1,10 @@
+// function addStudent(){
+//     let list      = document.group.list.value;
+//     let name      = document.group.name.value;
+//     let tableRow  = document.createElement('tr');
+//     let tabledata = tableRow.appendChild(document.createElement('td'));
+    
+//     tableRow.innerHTML = group;
+
+//     document.getElementsByClassName("table-list").appendChild(tableRow);
+// }
